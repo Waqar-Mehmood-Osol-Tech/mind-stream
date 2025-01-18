6cd7dc76e509f4da06f83a568fb3ef9c8c86d862
Waqar-Mehmood-Osol-Tech <waqar.mahmood1@osoltech.com>
2025-01-18 15:54:14 +0500

    Update API proxy target in Vite configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy:{
-      '/api' : {
-        target: 'http://localhost:3001',
-        secure: false
-      }
-    }
+    proxy: {
+      "/api": {
+        target: "https://mindstream-api.osolglobal.tech/",
+        secure: false,
+      },
+    },
   },
   plugins: [react()],
-})
+});
